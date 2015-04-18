f8779110368d79cda14380394c1fa0e8ddf9d6da
Joel Cool-Panama <mr.jkuhl@gmail.com>
2015-04-18 08:46:52 -0700

    Repository created.

---
+++ www/js/cipher.js
@@ -0,0 +1,90 @@
+function CipherObject(shift) {
+
+	// Need to handle spaces
+	this.encrypt = function(text) {
+
+		text = text.toLowerCase();
+		var i = 0;
+		var j = 0;
+		var cipherText = new Array();
+
+		while (i < text.length) {
+
+		  while (j < this.cipher.length) {
+
+		    if(text[i] == this.alphabet[j]) {
+
+			window.alert("Encrypting... " + text[i] + " => " + this.cipher[j]);
+		      cipherText[i] = this.cipher[j];
+		      j = 0;
+		      break;
+		    }
+		    j++;
+		  }
+		  i++;
+		}
+
+		return cipherText;
+	}
+
+	// Need to handle spaces
+	this.decrypt = function(cipherText) {
+
+		var i = 0;
+		var j = 0;
+		var text = new Array();
+
+		while (i < cipherText.length) {
+
+		  while (j < this.cipher.length) {
+
+		    if(cipherText[i] == this.cipher[j]) {
+
+			window.alert("Decrypting... " + cipherText[i] + " => " + this.alphabet[j]);
+		      text[i] = this.alphabet[j];
+		      j = 0;
+		      break;
+		    }
+		    j++;
+		  }
+		  i++;
+		}
+
+		return text
+	}
+
+	this.setCipher = function() {
+
+		var i = 0;
+		var cipher = new Array(this.alphabet.length);
+
+		while (i < this.alphabet.length) {
+
+			cipher[i] = this.alphabet[(i+this.shift)%this.alphabet.length];
+
+			i++;
+		}
+
+		this.cipher = cipher;
+	}
+
+	this.setShift = function(shift) {
+
+		this.shift = shift;
+		this.setCipher();
+	}
+
+	this.alphabet=[
+		'a', 'b', 'c', 'd', 'e',
+		'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y',
+		'z'
+	];
+
+	this.shift = shift;
+	this.cipher;
+
+	this.setCipher();
+}
